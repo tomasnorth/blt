b43df2e7abe9e1e984936edb46374549b37ae240
Tomas <27066503+creatort@users.noreply.github.com>
2021-08-07 21:57:37 +0100

    remove console log because bleh

--- blt_chrome_plugin/background.js
+++ blt_chrome_plugin/background.js
@@ -1,6 +1,5 @@
 chrome.runtime.onInstalled.addListener(async () => {
   let url = chrome.runtime.getURL("index.html");
   let tab = await chrome.tabs.create({ url });
-  console.log(`TomasTab is running on ${tab.id}`);
 });
 
